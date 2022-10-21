c691157f8866aca6e12bb3f974ed7a91cb133d18
Jacobschoenbauer <jhwt65@gmail.com>
2022-10-21 10:19:06 -0500

    added id by hour and start javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,17 @@
+//i open planner
+// the current day is displayed at the top of calendar
+// when scroll down presented with blocks for standard buisness hours
+// i view the time blocks for standard buisness hours
+//each time block is color coded to indicate whether it is in the past present future
+//click block can enter event
+// click button save button for the block the textfor the event is savedin local storage
+// i refresh page and events stay on page
+
 let today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
+function timeTracker() {
+  var timeNow = moment().hour();
 
+  
+}
