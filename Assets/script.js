b798318f54d35af8cfec15f3778e1eecbd128e27
Jacobschoenbauer <jhwt65@gmail.com>
2022-10-23 17:43:45 -0500

    javascript and css worked on

--- Assets/script.js
+++ Assets/script.js
@@ -6,6 +6,20 @@
 //click block can enter event
 // click button save button for the block the textfor the event is savedin local storage
 // i refresh page and events stay on page
+const localInfo1 = document.querySelector(".hourinfo7");
+const localInfo2 = document.querySelector(".hourinfo8");
+const localInfo3 = document.querySelector(".hourinfo9");
+const localInfo4 = document.querySelector(".hourinfo10");
+const localInfo5 = document.querySelector(".hourinfo11");
+const localInfo6 = document.querySelector(".hourinfo12");
+const localInfo7 = document.querySelector(".hourinfo13");
+const localInfo8 = document.querySelector(".hourinfo14");
+const localInfo9 = document.querySelector(".hourinfo15");
+const localInfo10 = document.querySelector(".hourinfo16");
+const localInfo11 = document.querySelector(".hourinfo17");
+const localInfo12 = document.querySelector(".hourinfo18");
+const localInfo13 = document.querySelector(".hourinfo19");
+const localInfo14 = document.querySelector(".hourinfo20");
 
 let today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY, h:mm a"));
@@ -14,26 +28,11 @@ let allRowArray = document.querySelectorAll(".shift-time");
 console.log("Here is: ", allRowArray);
 for (i = 0; i < allRowArray.length; i++) {
   console.log($(allRowArray[i]).attr("id"));
-
+  var waterYou = $(allRowArray[i]).attr("id");
   $(".button").on("click", function () {
     var text = $(this).siblings(".thingstodo").val();
     var time = $(this).parent().attr("id");
     window.localStorage.setItem(time, text);
-    const localInfo1 = document.querySelector(".hourinfo7");
-    const localInfo2 = document.querySelector(".hourinfo8");
-    const localInfo3 = document.querySelector(".hourinfo9");
-    const localInfo4 = document.querySelector(".hourinfo10");
-    const localInfo5 = document.querySelector(".hourinfo11");
-    const localInfo6 = document.querySelector(".hourinfo12");
-    const localInfo7 = document.querySelector(".hourinfo13");
-    const localInfo8 = document.querySelector(".hourinfo14");
-    const localInfo9 = document.querySelector(".hourinfo15");
-    const localInfo10 = document.querySelector(".hourinfo16");
-    const localInfo11 = document.querySelector(".hourinfo17");
-    const localInfo12 = document.querySelector(".hourinfo18");
-    const localInfo13 = document.querySelector(".hourinfo19");
-    const localInfo14 = document.querySelector(".hourinfo20");
-
     localInfo1.textContent = localStorage.getItem(7);
     localInfo2.textContent = localStorage.getItem(8);
     localInfo3.textContent = localStorage.getItem(9);
@@ -50,22 +49,22 @@ for (i = 0; i < allRowArray.length; i++) {
     localInfo14.textContent = localStorage.getItem(20);
   });
   $(".clearbutton").on("click", function () {
-    window.localStorage.clear()
+    window.localStorage.clear();
     window.location.reload();
-
   });
- 
-  var waterYou = $(allRowArray[i]).attr("id");
+}
 
-  function timeTracker() {
-    let timeNow = moment().hour();
-    console.log(timeNow);
+function timeTracker() {
+  let timeNow = moment().hour();
+  console.log(timeNow);
+  for (i = 0; i < allRowArray.length; i++) {
+    let allRowArray = document.querySelectorAll(".shift-time");
     $(".shift-time").each(function () {
-      if (parseInt(waterYou) > timeNow) {
+      if ($(allRowArray[i]).attr("id") > timeNow) {
         $(this).removeClass("future");
         $(this).removeClass("present");
         $(this).addClass("past");
-      } else if (parseInt(waterYou).attr("id") == timeNow) {
+      } else if ($(allRowArray[i]).attr("id") === timeNow) {
         $(this).removeClass("past");
         $(this).removeClass("future");
         $(this).addClass("present");
@@ -74,8 +73,25 @@ for (i = 0; i < allRowArray.length; i++) {
         $(this).removeClass("past");
         $(this).addClass("future");
       }
+      if ($(allRowArray[7]).attr("id") === waterYou) {
+        let numTime = 7;
+      }
     });
   }
 }
-
 timeTracker();
+
+localInfo1.textContent = localStorage.getItem(7);
+localInfo2.textContent = localStorage.getItem(8);
+localInfo3.textContent = localStorage.getItem(9);
+localInfo4.textContent = localStorage.getItem(10);
+localInfo5.textContent = localStorage.getItem(11);
+localInfo6.textContent = localStorage.getItem(12);
+localInfo7.textContent = localStorage.getItem(13);
+localInfo8.textContent = localStorage.getItem(14);
+localInfo9.textContent = localStorage.getItem(15);
+localInfo10.textContent = localStorage.getItem(16);
+localInfo11.textContent = localStorage.getItem(17);
+localInfo12.textContent = localStorage.getItem(18);
+localInfo13.textContent = localStorage.getItem(19);
+localInfo14.textContent = localStorage.getItem(20);
