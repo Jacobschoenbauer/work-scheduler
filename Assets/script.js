7630f1558ca879e589375ebda4dc5dc7d9590f3c
Jacobschoenbauer <jhwt65@gmail.com>
2022-10-23 14:43:52 -0500

    saved items on local storage and clear button

--- Assets/script.js
+++ Assets/script.js
@@ -19,33 +19,49 @@ for (i = 0; i < allRowArray.length; i++) {
     var text = $(this).siblings(".thingstodo").val();
     var time = $(this).parent().attr("id");
     window.localStorage.setItem(time, text);
-   
-    
+    const localInfo1 = document.querySelector(".hourinfo7");
+    const localInfo2 = document.querySelector(".hourinfo8");
+    const localInfo3 = document.querySelector(".hourinfo9");
+    const localInfo4 = document.querySelector(".hourinfo10");
+    const localInfo5 = document.querySelector(".hourinfo11");
+    const localInfo6 = document.querySelector(".hourinfo12");
+    const localInfo7 = document.querySelector(".hourinfo13");
+    const localInfo8 = document.querySelector(".hourinfo14");
+    const localInfo9 = document.querySelector(".hourinfo15");
+    const localInfo10 = document.querySelector(".hourinfo16");
+    const localInfo11 = document.querySelector(".hourinfo17");
+    const localInfo12 = document.querySelector(".hourinfo18");
+    const localInfo13 = document.querySelector(".hourinfo19");
+    const localInfo14 = document.querySelector(".hourinfo20");
+
+    localInfo1.textContent = localStorage.getItem(7);
+    localInfo2.textContent = localStorage.getItem(8);
+    localInfo3.textContent = localStorage.getItem(9);
+    localInfo4.textContent = localStorage.getItem(10);
+    localInfo5.textContent = localStorage.getItem(11);
+    localInfo6.textContent = localStorage.getItem(12);
+    localInfo7.textContent = localStorage.getItem(13);
+    localInfo8.textContent = localStorage.getItem(14);
+    localInfo9.textContent = localStorage.getItem(15);
+    localInfo10.textContent = localStorage.getItem(16);
+    localInfo11.textContent = localStorage.getItem(17);
+    localInfo12.textContent = localStorage.getItem(18);
+    localInfo13.textContent = localStorage.getItem(19);
+    localInfo14.textContent = localStorage.getItem(20);
   });
+  $(".clearbutton").on("click", function () {
+    window.localStorage.clear()
+    window.location.reload();
 
- console.log(localStorage.getItem(7));
- localStorage.getItem(7)
- localStorage.getItem(8)
- localStorage.getItem(9)
- localStorage.getItem(10)
- localStorage.getItem(11)
- localStorage.getItem(12)
- localStorage.getItem(13)
- localStorage.getItem(15)
- localStorage.getItem(16)
- localStorage.getItem(17)
- localStorage.getItem(18)
- localStorage.getItem(19)
- localStorage.getItem(20)
+  });
  
+  console.log(localStorage.getItem(7));
 
- var waterYou = $(allRowArray[i]).attr("id")
-
-
+  var waterYou = $(allRowArray[i]).attr("id");
 
   function timeTracker() {
     let timeNow = moment().hour();
-    console.log(timeNow)
+    console.log(timeNow);
     $(".shift-time").each(function () {
       if ($(allRowArray[i]).attr("id") > timeNow) {
         $(this).removeClass("future");
@@ -61,13 +77,7 @@ for (i = 0; i < allRowArray.length; i++) {
         $(this).addClass("future");
       }
     });
-   
   }
 }
-
-
-
-
-
 
 timeTracker();
